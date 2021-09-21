7c9d9805d08470cae79c628353401d4903c0bc1e
bewipib777@mailart.ws <bewipib777@mailart.ws>
2021-09-21 16:41:51 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Airo auto = new Airo("Аэромобиль");
+            Pyatna pyatno = new Pyatna("пятна");
+            
+            Console.WriteLine("{0} {1} {2} {3}",auto.name, auto.heightSpeed(), pyatno.name, pyatno.prostupat());
+    
+            Console.ReadKey();
+        }
+    }
+
+    public class Airo
+    {
+        public string name { get; set; }
+
+        public Airo(string name)
+        {
+            this.name = name;
+        }
+
+        public string heightSpeed()
+        {
+            return ", со скоростью, превышающей R17, вынес их по стальному туннелю на хмурую поверхность Магратеи, объятую мглистыми предрассветными сумерками.";
+        }
+
+
+    }
+
+    public class Pyatna
+    {
+        public string name { get; set; }
+
+        public Pyatna(string name)
+        {
+            this.name = name;
+        }
+
+        public string prostupat()
+        {
+            return "Сквозь тьму уже проступали пятна призрачного серого света.";
+        }
+
+    }
+
+}
---
+++ TestUnit3/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Test3;
+
+namespace TestUnit3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Airo auto = new Airo("Аэромобиль");
+            Pyatna pyatno = new Pyatna("пятна");
+
+            Assert.AreEqual("Аэромобиль", auto.name);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Airo auto = new Airo("Аэромобиль");
+            Assert.AreEqual(", со скоростью, превышающей R17, вынес их по стальному туннелю на хмурую поверхность Магратеи, объятую мглистыми предрассветными сумерками.", auto.heightSpeed());
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Pyatna pyatno = new Pyatna("пятна");
+            Assert.AreEqual(pyatno.name, "пятна");
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Pyatna pyatno = new Pyatna("пятна");
+            Assert.AreEqual(pyatno.prostupat(), "Сквозь тьму уже проступали пятна призрачного серого света.");
+        }
+    }
+}
